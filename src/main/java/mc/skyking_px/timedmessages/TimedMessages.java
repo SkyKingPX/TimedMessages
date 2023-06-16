dd2c3a91fea424e76763598c6e9f16388b89b514
Antony Blem <antony.blem@icloud.com>
2023-06-16 08:12:12 +0200

    Different changes and improvemets
    Renamed Package

--- src/main/java/mc/skyking_px/timedmessages/TimedMessages.java
+++ src/main/java/mc/skyking_px/timedmessages/TimedMessages.java
@@ -1,6 +1,7 @@
-package mc.timed_messages.timedmessages;
+package mc.skyking_px.timedmessages;
 
 
+import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
@@ -25,10 +26,16 @@ public final class TimedMessages extends JavaPlugin {
     }
 
     //Version control
-    double plugin_version = 2.0;
+    double plugin_version = 0.1;
+
+
 
     @Override
     public void onEnable() {
+
+        int pluginId = 18759;
+        Metrics metrics = new Metrics(this, pluginId);
+
         // Plugin startup logic
 
         this.saveDefaultConfig();
@@ -59,28 +66,28 @@ public final class TimedMessages extends JavaPlugin {
         long period = getConfig().getLong("period");
 
         System.out.println( "\n[]=====[Enabling TimedMessages]=====[]\n" +
-                            "| Information:\n" +
-                            "|   Name: TimedMessages\n" +
-                            "|   Developer: SkyKing_PX\n" +
-                            "|   Version: " + plugin_version + "\n" +
-                            "| Support:\n" +
-                            "|   Discord: SkyKing_PX#3612\n" +
-                            "|   GitHub: https://bit.ly/3ZZ8cCF\n" +
-                            "[]==================================[]\n");
+                "| Information:\n" +
+                "|   Name: TimedMessages\n" +
+                "|   Developer: SkyKing_PX\n" +
+                "|   Version: " + plugin_version + "\n" +
+                "| Support:\n" +
+                "|   Discord: SkyKing_PX#3612\n" +
+                "|   GitHub: https://bit.ly/3ZZ8cCF\n" +
+                "[]==================================[]\n");
 
         for (String msg : this.getConfig().getStringList("messages")){
-        List<String> messages = new ArrayList<>();
-        messages.add(ChatColor.translateAlternateColorCodes('&', msg ));
-        //messages.add("§l§4[§6Warnung§4] §r§lWenn du die Regeln nicht befolgst kommt es zu einem §cBan!");
-        //messages.add("§l§4[§6Warnung§4] §r§lLasse keine AFK-Farmen eingeschaltet, wenn du aus den Chunks gehst oder der Server gestoppt/neugestartet wird!");
-        //messages.add("§l§4[§9Tipp§4] §r§lDie Spielregel Inventar behalten ist §caktiviert§r§l!");
-        //messages.add("§l§4[§9Tipp§4] §r§lVerzaubere deine Schuhe mit §cSeelenläufer§r§l(§cSoul Speed§r§l), um sich im Nether-Tunnel §cschneller §r§lzu bewegen!");
-        //messages.add("§l§4[§9Tipp§4] §r§lVerzaubere deinen Dreizack mit §cSog§r§l(§cRiptide§r§l), um in der Oberwelt schneller zu Reisen!");
-        //messages.add("§l§4[§9Tipp§4] §r§lGucke dir die WebMap von jedem Server an:§r§9§n zusammenhub.myftp.org:25564");
-        //messages.add("§l§4[§2Werbung§4] §r§lMöchtest du ein Projekt mit deinen Freunden starten? Hoste Deinen kostenlosen Server: https://github.com/SkyKingPX/Minecraft-Server-Projekte/issues/new");
+            List<String> messages = new ArrayList<>();
+            messages.add(ChatColor.translateAlternateColorCodes('&', msg ));
+            //messages.add("§l§4[§6Warnung§4] §r§lWenn du die Regeln nicht befolgst kommt es zu einem §cBan!");
+            //messages.add("§l§4[§6Warnung§4] §r§lLasse keine AFK-Farmen eingeschaltet, wenn du aus den Chunks gehst oder der Server gestoppt/neugestartet wird!");
+            //messages.add("§l§4[§9Tipp§4] §r§lDie Spielregel Inventar behalten ist §caktiviert§r§l!");
+            //messages.add("§l§4[§9Tipp§4] §r§lVerzaubere deine Schuhe mit §cSeelenläufer§r§l(§cSoul Speed§r§l), um sich im Nether-Tunnel §cschneller §r§lzu bewegen!");
+            //messages.add("§l§4[§9Tipp§4] §r§lVerzaubere deinen Dreizack mit §cSog§r§l(§cRiptide§r§l), um in der Oberwelt schneller zu Reisen!");
+            //messages.add("§l§4[§9Tipp§4] §r§lGucke dir die WebMap von jedem Server an:§r§9§n zusammenhub.myftp.org:25564");
+            //messages.add("§l§4[§2Werbung§4] §r§lMöchtest du ein Projekt mit deinen Freunden starten? Hoste Deinen kostenlosen Server: https://github.com/SkyKingPX/Minecraft-Server-Projekte/issues/new");
 
-        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> Bukkit.broadcastMessage(messages.get(new Random().nextInt(messages.size()))), 0L, 20L * period); //standard: 20L * 600L - testing: 20L * 20L
-    }
+            Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> Bukkit.broadcastMessage(messages.get(new Random().nextInt(messages.size()))), 0L, 20L * period); //standard: 20L * 600L - testing: 20L * 20L
+        }
     }
 
     @Override
@@ -88,14 +95,14 @@ public final class TimedMessages extends JavaPlugin {
         // Plugin shutdown logic
 
         System.out.println( "\n[]=====[Disabling TimedMessages]=====[]\n" +
-                            "| Information:\n" +
-                            "|   Name: TimedMessages\n" +
-                            "|   Developer: SkyKing_PX\n" +
-                            "|   Version: " + plugin_version + "\n" +
-                            "| Support:\n" +
-                            "|   Discord: SkyKing_PX#3612\n" +
-                            "|   GitHub: https://bit.ly/3ZZ8cCF\n" +
-                            "[]===================================[]\n");
+                "| Information:\n" +
+                "|   Name: TimedMessages\n" +
+                "|   Developer: SkyKing_PX\n" +
+                "|   Version: " + plugin_version + "\n" +
+                "| Support:\n" +
+                "|   Discord: SkyKing_PX#3612\n" +
+                "|   GitHub: https://bit.ly/3ZZ8cCF\n" +
+                "[]===================================[]\n");
     }
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -121,42 +128,42 @@ public final class TimedMessages extends JavaPlugin {
                         return true;
                     }
 
-                    }
-
                 }
 
             }
-            if (!sender.hasPermission("tm.settimedmessage")) {
 
-                sender.sendMessage(ChatColor.RED + "You have not the permission to run this command!");
+        }
+        if (!sender.hasPermission("tm.settimedmessage")) {
+
+            sender.sendMessage(ChatColor.RED + "You have not the permission to run this command!");
+            return true;
+
+        } else if (sender.hasPermission("tm.settimedmessage")) {
+
+            if (args.length == 0) {
+
+                sender.sendMessage(ChatColor.AQUA + "Usage: /tm reloadconfig|settmmsg|setperiod");
                 return true;
 
-            } else if (sender.hasPermission("tm.settimedmessage")) {
+            }
+            if (args.length > 0) {
 
-                if (args.length == 0) {
+                if (args[0].equalsIgnoreCase("settmmsg")) {
+                    String messages = args[1].toString();
+                    if (!args[1].isEmpty()){
 
-                    sender.sendMessage(ChatColor.AQUA + "Usage: /tm reloadconfig|settmmsg|setperiod");
-                    return true;
+                        try {
+                            this.getConfig().set(messages , "messages");
+                            this.saveDefaultConfig();
+                        }catch (Exception exception) {
 
-                }
-                if (args.length > 0) {
+                        }
 
-                    if (args[0].equalsIgnoreCase("settmmsg")) {
-                        String messages = args[1].toString();
-                        if (!args[1].isEmpty()){
-
-                            try {
-                                this.getConfig().set(messages , "messages");
-                                this.saveDefaultConfig();
-                            }catch (Exception exception) {
-
-                            }
-
-                            return true;
+                        return true;
 
 
-                        }else {
-                            sender.sendMessage(ChatColor.AQUA + "Usage: /tm settmmsg <message>");
+                    }else {
+                        sender.sendMessage(ChatColor.AQUA + "Usage: /tm settmmsg <message>");
                     }
 
                 }
@@ -180,9 +187,9 @@ public final class TimedMessages extends JavaPlugin {
                         String period;
                         if (!args[1].isEmpty()){
 
-                            Long.parseLong(args[1]);
+                            //Long.parseLong(args[1]);
 
-                            if (args[1].) {
+                            if (args[1] = ) {
 
                                 period = args[1];
                                 this.getConfig().set(period , "period");
@@ -207,7 +214,8 @@ public final class TimedMessages extends JavaPlugin {
 
             }
 
-            }return false;
+        }return false;
     }
 
 }
+
